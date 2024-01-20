9684635e215ece9dd20e76f948761b538d71adc2
yosshii8888 <138771166+yosshii8888@users.noreply.github.com>
2024-01-20 14:33:23 +0900

    spr_camera
    Arduino最高！

--- test_code/sprs_camera/README.md
+++ test_code/sprs_camera/README.md
@@ -1,1 +1,3 @@
 # Spresenseのカメラを読み取るプログラムです
+.inoはSpresenseに書き込み
+show_img.pyはWindowsで別に起動、受信用
---
+++ test_code/sprs_camera/show_img.py
@@ -0,0 +1,33 @@
+import serial
+import base64
+import cv2
+import numpy as np
+
+with serial.Serial('COM4',1000000, timeout=3) as reader:
+    count = 0
+
+    readingBase64 = False
+    list = []
+    while True:
+        line = reader.readline().decode('ascii').strip()
+        
+        # 画像部分を切り出す
+        if line.startswith('#Image'):
+            readingBase64 = True
+            continue
+        if line.startswith('#End'):
+            buffer = base64.b64decode(''.join(list));
+            list.clear()
+            data = np.frombuffer(buffer, dtype=np.uint8)
+            image = cv2.imdecode(data, cv2.IMREAD_COLOR)
+            cv2.imshow("Camera",image)
+            cv2.waitKey(1) 
+            # count = count + 1
+            readingBase64 = False
+            continue
+
+        if readingBase64:
+            list.append(line)
+        else:
+            print(line)
+    cv2.destroyAllWindows()

640646fafcee8777795ff46523986dfcd34d1853
Sayuri-Miyake <lilium.mentha@gmail.com>
2024-01-20 15:49:50 +0900

    class

---
+++ bbm/hcsr04/hcsr04.hpp
@@ -0,0 +1,11 @@
+#ifndef SC19_PICO_HCSR04_HPP_
+#define SC19_PICO_HCSR04_HPP_
+
+// Class declaration
+class HCSR04 {
+public:
+    int gettingTime();          
+    int gotTime;
+};
+
+#endif // SC19_PICO_HCSR04_HPP_

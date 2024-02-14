0c01e72d6cc37ae3c27961fbc19e58a24f6cf441
kasuta <poguxiang@gmail.com>
2024-02-15 00:29:27 +0900

    加速度を別々に表示できるようにする(テスト)

--- test_code/bno055/BNO055.cpp
+++ test_code/bno055/BNO055.cpp
@@ -8,8 +8,8 @@ static int addr = 0x28;
 
 // Initialise Accelerometer Function
 void accel_init(void){
-    // Check to see if connection is correct
-    sleep_ms(1000); // Add a short delay to help BNO005 boot up
+    // 接続が正常か確かめる
+    sleep_ms(1000);
     uint8_t reg = 0x00;
     uint8_t chipID[1];
     i2c_write_blocking(I2C_PORT, addr, &reg, 1, true);
@@ -55,11 +55,22 @@ void accel_init(void){
     i2c_write_blocking(I2C_PORT, addr, data, 2, true);
     sleep_ms(30);
 
-    // Set operation to AMG(Accel Mag Gyro)
+    // //モードをAMG(Accel Mag Gyro)に設定
+    // data[0] = 0x3D;
+    // data[1] = 0b0111;
+    // i2c_write_blocking(I2C_PORT, addr, data, 2, true);
+    // sleep_ms(100);
+
+    //モードをNDOFに設定
+    //メモ:NDOFは加速度を分けれるFusion modeに対応している
+    //また、Fusion Dataは地球の補正が入ったAbsolute orientationになっている
+    //解釈違いがあるかもなので詳しくはData Sheet p21~
     data[0] = 0x3D;
-    data[1] = 0b0111;
+    data[1] = 0b1100;
     i2c_write_blocking(I2C_PORT, addr, data, 2, true);
     sleep_ms(100);
+
+    //メモ:SC-17のコードでもdata[1] = 0x0C = (int)12となっている??
 }
 
 int main(void){
@@ -75,21 +86,37 @@ int main(void){
     // Call accelerometer initialisation function
     accel_init();
 
-    //加速度
-    uint8_t accel[6]; // Store data from the 6 acceleration registers
-    int16_t accelX, accelY, accelZ; // Combined 3 axis data
-    float f_accelX, f_accelY, f_accelZ; // Float type of acceleration data
-    uint8_t accel_val = 0x08; // Start register address
+    //全加速度
+    // uint8_t accel[6]; // Store data from the 6 acceleration registers
+    // int16_t accelX, accelY, accelZ; // Combined 3 axis data
+    // float f_accelX, f_accelY, f_accelZ; // Float type of acceleration data
+    // uint8_t accel_val = 0x08; // ACC_DATA_XLSB 0x08
+
     //磁気
     uint8_t mag[6];   // magnetometer registers
     int16_t magX, magY, magZ;
     float f_magX, f_magY, f_magZ;
-    uint8_t mag_val = 0x0E;
+    uint8_t mag_val = 0x0E; //MAG_DATA_XLSB 0x0E
+
     //ジャイロセンサ
     uint8_t gyro[6];
     int16_t gyroX, gyroY, gyroZ;
     float f_gyroX, f_gyroY, f_gyroZ;
-    uint8_t gyro_val = 0x14;
+    uint8_t gyro_val = 0x14; //GYR_DATA_X_LSB 0x14
+
+    //重力加速度
+    uint8_t grv[6];
+    int16_t grvX, grvY, grvZ;
+    float f_grvX, f_grvY, f_grvZ;
+    uint8_t grv_val = 0x2E; //GRV_DATA_X_LSB 0x2E
+
+    //加速度
+    uint8_t accel[6];
+    int16_t accelX, accelY, accelZ;
+    float f_accelX, f_accelY, f_accelZ;
+    uint8_t accel_val = 0x28; //LIA_DATA_X_LSB 0x28  
+
+
     // Infinite Loop
     while(1){
         //read acceleration data
@@ -104,7 +131,20 @@ int main(void){
         f_accelY = accelY / 100.00;
         f_accelZ = accelZ / 100.00;
 
-            // Read magnetometer data
+
+        //read gravity data
+        i2c_write_blocking(I2C_PORT, addr, &grv_val, 1, true);
+        i2c_read_blocking(I2C_PORT, addr, grv, 6, false);
+
+        accelX = ((grv[1]<<8) | grv[0]);
+        accelY = ((grv[3]<<8) | grv[2]);
+        accelZ = ((grv[5]<<8) | grv[4]);
+
+        f_grvX = grvX / 100.00;
+        f_grvY = grvY / 100.00;
+        f_grvZ = grvZ / 100.00;
+
+        // Read magnetometer data
         i2c_write_blocking(I2C_PORT, addr, &mag_val, 1, true);
         i2c_read_blocking(I2C_PORT, addr, mag, 6, false);
         magX = ((mag[1] << 8) | mag[0]);
@@ -115,7 +155,7 @@ int main(void){
         f_magY = magY / 16.00;
         f_magZ = magZ / 16.00;
 
-            // Read gyro data
+        // Read gyro data
         i2c_write_blocking(I2C_PORT, addr, &gyro_val, 1, true);
         i2c_read_blocking(I2C_PORT, addr, gyro, 6, false);
         gyroX = ((gyro[1] << 8) | gyro[0]);
@@ -129,6 +169,8 @@ int main(void){
         // Print to serial monitor
         printf("accel");
         printf("X: %6.2f    Y: %6.2f    Z: %6.2f\n", f_accelX, f_accelY, f_accelZ);
+        printf("gravity");
+        printf("X: %6.2f    Y: %6.2f    Z: %6.2f\n", f_grvX, f_grvY, f_grvZ);
         printf("mag  ");
         printf("X: %6.2f    Y: %6.2f    Z: %6.2f\n", f_magX, f_magY, f_magZ);
         printf("gyro ");

4ff995629b9326ee1c47181488d20d7fa1d35c5d
n17y17 <N17Y17@outlook.com>
2024-02-18 01:04:08 +0900

    SCフォルダ内の一部を修正

--- bbm/sc/src/binary.cpp
+++ bbm/sc/src/binary.cpp
@@ -61,7 +61,7 @@ Binary Binary::operator+ (Binary other_binary) const
 
 void Binary::to_assign(uint8_t* reg_ptr) const
 {
-    for (int i=0; i< this->size(); ++i)
+    for (std::size_t i=0; i<_binary_data.size(); ++i)
     {
         *reg_ptr = _binary_data.at(i);
         ++reg_ptr;

6b2be70cddc6e1a91190c683ebd8073d3f80b796
n17y17 <N17Y17@outlook.com>
2024-02-18 00:55:20 +0900

    ビルドが通るように修正

--- bbm/bbm.hpp
+++ bbm/bbm.hpp
@@ -6,5 +6,8 @@
 #include "hcsr04/hcsr04.hpp"
 #include "bme280/bme280.hpp"
 #include "bno055/bno055_BBM.hpp"
+#include "njl5513r/njl5513r.hpp"
+#include "sd/sd.hpp"
+#include "speaker/speaker.hpp"
 
 #endif  // SC19_PICO_BBM_HPP_

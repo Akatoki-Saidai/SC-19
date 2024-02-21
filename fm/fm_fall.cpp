035cf4ee776b38ef2cc49a5bd75075d7723027a8
machidadada <tuttifrutti0416@gmail.com>
2024-02-21 15:25:00 +0900

    fallingfalling螺旋状に落ちていく摩天楼に

--- fm/fm_fall.cpp
+++ fm/fm_fall.cpp
@@ -9,9 +9,9 @@ void fall_fase(const BME280& bme280, const BNO055& bno055, const HCSR04& hcsr04,
 {
     int separate;
     Altitude<Unit::m> altitude(pressure, temperature);
-    if(altitude<1.0)//地面からの標高が１m以内
+    if(altitude<10.0)//地面からの標高が10m以内
     {
-        sleep(5_s);//念のため待機しておく
+        sleep(10_s);//念のため待機しておく
         separete=para_separate.read;
         if(separate!=1)//パラシュートが取れていない場合、動いてみる？
         {
@@ -19,16 +19,27 @@ void fall_fase(const BME280& bme280, const BNO055& bno055, const HCSR04& hcsr04,
          sleep(1_s);
          motor.forward(0);
          break;
-
         }
-         Acceleration<Unit::m_s2> gravity_acceleration = std::get<1>(bno_data);//重力加速度取得
-        if(gravity_acceleration.z()>0)//Z軸の重力加速度が正かどうかで機体の体制修正
+        Acceleration<Unit::m_s2> gravity_acceleration = std::get<1>(bno_data);//重力加速度取得
+        //Z軸の重力加速度が正かどうかで機体の体制修正
+        if(gravity_acceleration.z()>0)//z軸が正なら正常
         {
-
-        }
-
          fase=Fase::Ldistance;
          break;
+        }
+        else
+        {
+         motor.forward(1.0);//じたばたして体制修正できるかな？
+         sleep(1_s);
+         motor.forward(0);
+         motor.right(1.0); 
+         sleep(1_s);
+         motor.right(0); 
+         motor.left(1.0); 
+         sleep(1_s);
+         motor.left(0);
+         break;
+        }
     }
     else
     {
